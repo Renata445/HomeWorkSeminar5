9dd4ce2560f4db1f0b4b57e91d9b43fa33e14ad1
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-13 22:32:19 +0300

    Task_34

---
+++ Task_34/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine()), k = 0;
+int[] a = new int [n];
+for(int i = 0; i < n; i++)
+{
+    a[i] = new Random().Next(100, 1000);
+    Console.WriteLine($"{string.Join(", ", a[i])}");
+    if(a[i] % 2 == 0)
+    {
+        k++;
+    }
+}
+Console.WriteLine(k);
