d557da85f44c1a10475a6ab57e757afbbeb58f62
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-17 22:26:10 +0300

    Task_Gipoteza

---
+++ Task_Gipoteza/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите четное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+bool[] a = new bool [n];
+for(int i = 2; i < n; i++)
+{
+    a[i] = true;
+}
+for(int i = 2; i*i < n; i++)
+{
+    if(a[i])
+    {
+        for(int j = i + i; j < n; j += i)
+        {
+            a[j] = false;
+            i = 2;
+
+        }
+    }
+    while(!(a[i] && a[n-i]))
+    {
+        i++;
+    }
+    Console.WriteLine($"{i}, {n - i}");
+}
